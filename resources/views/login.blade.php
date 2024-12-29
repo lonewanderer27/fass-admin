3db1cd3d5290b4832f61ded983230c1bcbc272c3
lonewanderer27 <adrianejames27@gmail.com>
2024-12-30 04:19:32 +0800

    feat(Login): add toggle visibility for password input

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,30 +5,23 @@
     <div class="flex justify-center items-center h-screen">
         <form class="flex flex-col mx-auto xs:w-full sm:w-96 gap-2">
             <label class="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    class="h-4 w-4 opacity-70">
-                    <path
-                        d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path
-                        d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                </svg>
+                Email
                 <input type="text" class="grow" placeholder="Enter your Email" />
             </label>
             <label class="input input-bordered flex items-center gap-2">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    class="h-4 w-4 opacity-70">
-                    <path
-                        fill-rule="evenodd"
-                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                        clip-rule="evenodd" />
+                Password
+                <input id="password" type="password" class="grow" placeholder="Enter your Password" />
+                <svg id="eye--icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
-                <input type="password" class="grow" placeholder="Enter your Password" />
+                <svg id="eye-slash--icon" style="display: none" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                </svg>
             </label>
             <div class="flex justify-end">
                 <button class="btn" type="submit">
@@ -38,3 +31,36 @@
         </form>
     </div>
 </x-layout>
+
+<script>
+    const eyeIcon = document.getElementById("eye--icon");
+    const eyeCloseIcon = document.getElementById("eye-slash--icon");
+    const passInput = document.getElementById("password");
+
+    function showPass() {
+        eyeCloseIcon.style.display = "none";
+        eyeIcon.style.display = "block";
+        passInput.setAttribute("type", "text");
+    }
+
+    function hidePass() {
+        eyeCloseIcon.style.display = "block";
+        eyeIcon.style.display = "none";
+        passInput.setAttribute("type", "password");
+    }
+
+    function toggle() {
+        // check if pass input has an attribute of type=password
+        // if there is, add style=display:none to eyeCloseIcon
+        // then add style=display:block to eyeIcon
+        if (passInput.getAttribute("type") === "password") {
+            showPass()
+        } else {
+            hidePass()
+        }
+    }
+
+    // assign onClick to each svg
+    eyeIcon.addEventListener("click", toggle)
+    eyeCloseIcon.addEventListener("click", toggle)
+</script>
