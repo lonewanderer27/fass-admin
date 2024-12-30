c30132c36682ce6fb63626694a487cc45c20d6f8
lonewanderer27 <adrianejames27@gmail.com>
2024-12-30 20:00:37 +0800

    Add navigation links between Login and Sign-Up pages
    Updated the login view to include a "Sign Up" link for users without an account and the signup view to include a "Log In" link for existing users. Adjusted button layouts for consistency and improved user interface.

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -38,9 +38,10 @@
                           d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
                 </svg>
             </label>
-            <div class="flex justify-end">
-                <button class="btn" type="submit">
-                    Signup
+            <p class="text-end">Already have an account? <a href="/login" class="font-bold">Log In.</a></p>
+            <div class="flex justify-end mt-5">
+                <button class="btn w-full sm:w-auto" type="submit">
+                    Sign Up
                 </button>
             </div>
         </form>
