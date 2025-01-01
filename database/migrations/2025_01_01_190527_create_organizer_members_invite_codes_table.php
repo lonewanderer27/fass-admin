e956552c1ec0b53b4402d2e0a062c1fbd5a34734
lonewanderer27 <adrianejames27@gmail.com>
2025-01-02 03:18:57 +0800

    Add migrations for organizers and related entities
    Introduce migrations for organizers, organizer members, invite codes, and their relationships. These include creating necessary tables along with foreign key constraints and ensuring data integrity via unique constraints. Added invite_code_id to organizer_members for linking members with invite codes.

---
+++ database/migrations/2025_01_01_190527_create_organizer_members_invite_codes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('organizer_members_invite_codes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('organizer_id')->constrained()->onDelete('cascade');
+            $table->string('invite_code')->unique();
+            $table->enum('status', ['active', 'used', 'invalid'])->default('active');
+            $table->timestamps();
+
+            // Composite unique constraint
+            $table->unique(['organizer_id', 'invite_code']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('organizer_members_invite_codes');
+    }
+};
