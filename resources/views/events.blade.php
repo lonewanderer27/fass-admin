218a5cf14417db2f1f211471079af34b260ab064
lonewanderer27 <adrianejames27@gmail.com>
2025-01-03 20:52:37 +0800

    ui(Events): show organizer for each event

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,17 +1,19 @@
-<x-layout title="Events" :showNavBar="true">
+<x-layout title="Events" :show_navbar="true">
     <x-slot:heading>
         Events
     </x-slot:heading>
-    <h1 class="text-2xl">Events</h1>
     @foreach($events as $event)
-        <div>
-            <p>
-                <a href="/events/{{ $event['id'] }}" class="hover:underline hover:text-blue-500">{{ $event["name"] }}</a>
-            </p>
-            <p>{{ $event["description"] }}</p>
-            <p>{{ $event["date"] }}</p>
-            <p>{{ $event["time"] }}</p>
-            <p>{{ $event["location"] }}</p>
+        <div class="card bg-base-100">
+            <div class="card-body">
+                <h3 class="card-title">
+                    <a href="/events/{{ $event['id'] }}" class="hover:underline hover:text-blue-500">{{ $event["name"] }}</a>
+                </h3>
+                <a class="font-bold text-blue-500" href="/organizers/{{ $event->organizer->id }}">{{ $event->organizer->name  }}</a>
+                <p>{{ $event["description"] }}</p>
+                <p>{{ $event["date"] }}</p>
+                <p>{{ $event["time"] }}</p>
+                <p>{{ $event["location"] }}</p>
+            </div>
         </div>
     @endforeach
 </x-layout>
