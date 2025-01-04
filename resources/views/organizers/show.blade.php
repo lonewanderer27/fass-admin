b5f4a05bf2091a6aba19baf6bffb08b141cdf93d
lonewanderer27 <adrianejames27@gmail.com>
2025-01-04 22:16:53 +0800

    feat: enhance organizer details view with additional info
    Updated the organizer's page to display more details, including description and email if available. Added an 'Edit' button for easier navigation to the edit page. Improved the overall layout for clearer presentation.

--- resources/views/organizers/show.blade.php
+++ resources/views/organizers/show.blade.php
@@ -1,9 +1,17 @@
-<x-layout title="Event" :show_navbar="true" :show_heading="true">
+<x-layout title="{{$organizer->name}}" :show_navbar="true" :show_heading="true">
     <x-slot:heading>
-        Organizer
+        {{ $organizer["name"] }}
     </x-slot:heading>
-    <div>
-        <p>{{ $organizer["name"] }}</p>
-        <p>{{ $organizer["phone_no"] }}</p>
+    <div class="p-6 lg:p-8">
+        @if($organizer->description)
+            <p>{{ $organizer["description"] }}</p>
+        @endif
+        <p><strong>Phone No:</strong> {{ $organizer["phone_no"] }}</p>
+        @if($organizer->email)
+            <p><strong>Email:</strong> {{ $organizer["email"] }}</p>
+        @endif
+        <div class="mt-6 flex items-center gap-x-6">
+            <a class="btn  btn-secondary" href="/organizers/{{$organizer->id}}/edit">Edit</a>
+        </div>
     </div>
 </x-layout>
