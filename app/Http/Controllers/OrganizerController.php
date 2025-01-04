a512eff129187fa851e8474f3e74c7b3187b0736
lonewanderer27 <adrianejames27@gmail.com>
2025-01-04 22:37:09 +0800

    refactor: create controller for organizer methods

---
+++ app/Http/Controllers/OrganizerController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Organizer;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class OrganizerController extends Controller
+{
+    // page: all organizers
+    public function index(): View
+    {
+        $organizers = Organizer::paginate(9);
+
+        return view('organizers.index', [
+            'organizers' => $organizers
+        ]);
+    }
+
+    // page: single organizer
+    public function show(Organizer $org): View
+    {
+        $members = $org->members()->take(3)->get();
+        $events = $org->events()->take(3)->get();
+
+        return view('organizers.show', [
+            'organizer' => $org,
+            'members' => $members,
+            'events' => $events
+        ]);
+    }
+
+    // form: create an organizer
+    public function create(): View
+    {
+        return view('organizers.create');
+    }
+
+    // form: edit an organizer
+    public function edit(Organizer $org): View
+    {
+        return view('organizers.edit', [
+            'organizer' => $org
+        ]);
+    }
+
+    // action: new organizer
+    public function store(Request $request): RedirectResponse
+    {
+        // validate request
+        $request->validate([
+            'name' => ['required', 'unique:organizers', 'min:3'],
+            'phone_no' => ['required', 'size:10'],
+            'description' => ['nullable'],
+            'email' => ['nullable'],
+            'avatar_url' => ['nullable'],
+            'cover_url' => ['nullable']
+        ]);
+
+        // filter out the null values
+        $filtered_values = array_filter($request->all(), fn($val) => !is_null($val));
+
+        // create the new organizer
+        Organizer::create($filtered_values);
+
+        // return and redirect to organizers page
+        return redirect('/organizers');
+    }
+
+    // action: update an organizer
+    public function update(Organizer $org): RedirectResponse
+    {
+        // validate the request
+        request()->validate([
+            // third param allows us to exclude the rule of unique to the org id
+            // which allows the org name to stay the same
+            // in case the user decided to not edit it
+            'name' => ['required',  'unique:organizers,name,' . $org->id, 'min:3'],
+            'phone_no' => ['required', 'size:10'],
+            'description' => ['nullable'],
+            'email' => ['nullable'],
+            'avatar_url' => ['nullable'],
+            'cover_url' => ['nullable']
+        ]);
+
+        // TODO: authorize the edit organizer request
+
+        // filter out the null values
+        $filtered_values = array_filter(request()->all(), fn($val) => !is_null($val));
+
+        // actually update the organizer
+        $org->updateOrFail($filtered_values);
+
+        // return and redirect to the organizer page
+        return redirect("/organizers/$org->id");
+    }
+
+    // action: delete an organizer
+    public function destroy(Organizer $org): RedirectResponse
+    {
+        // TODO: authorize the delete organizer request
+
+        // actually delete the organization
+        $org->deleteOrFail();
+
+        // return and redirect to organizers page
+        return redirect('/organizers');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\OrganizerController;
 use App\Models\Event;
-use App\Models\Organizer;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 
@@ -9,9 +9,7 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
+Route::view('/welcome', 'welcome');
 
 Route::get('/login', function () {
     return view('login');
@@ -38,90 +36,15 @@ Route::get('/events', function () {
     ]);
 });
 
-Route::get('/organizers', function() {
-    $organizers = Organizer::paginate(9);
-
-    return view('organizers.index', [
-        'organizers' => $organizers
-    ]);
-});
-
-Route::get('/organizers/create', function() {
-    return view('organizers.create');
-});
-
-Route::post('/organizers', function() {
-    // validate request
-    request()->validate([
-        'name' => ['required', 'unique:organizers', 'min:3'],
-        'phone_no' => ['required', 'size:10'],
-        'description' => ['nullable'],
-        'email' => ['nullable'],
-        'avatar_url' => ['nullable'],
-        'cover_url' => ['nullable']
-    ]);
-
-    // filter out the null values
-    $filtered_values = array_filter(request()->all(), fn($val) => !is_null($val));
-
-    // create the new organizer
-    Organizer::create($filtered_values);
-
-    // return and redirect to organizers page
-    return redirect('/organizers');
-});
-
-Route::delete('/organizers/{org}', function (Organizer $org) {
-    // TODO: authorize the delete organizer request
-
-    // actually delete the organization
-    $org->deleteOrFail();
-
-    // return and redirect to organizers page
-    return redirect('/organizers');
-});
-
-Route::patch('/organizers/{org}', function (Organizer $org) {
-    // validate the request
-    request()->validate([
-        // third param allows us to exclude the rule of unique to the org id
-        // which allows the org name to stay the same
-        // in case the user decided to not edit it
-        'name' => ['required',  'unique:organizers,name,' . $org->id, 'min:3'],
-        'phone_no' => ['required', 'size:10'],
-        'description' => ['nullable'],
-        'email' => ['nullable'],
-        'avatar_url' => ['nullable'],
-        'cover_url' => ['nullable']
-    ]);
-
-    // TODO: authorize the edit organizer request
-
-    // filter out the null values
-    $filtered_values = array_filter(request()->all(), fn($val) => !is_null($val));
-
-    // actually update the organizer
-    $org->updateOrFail($filtered_values);
-
-    // return and redirect to the organizer page
-    return redirect("/organizers/$org->id");
-});
-
-Route::get('/organizers/{org}', function (Organizer $org) {
-    $members = $org->members()->take(3)->get();
-    $events = $org->events()->take(3)->get();
-
-    return view('organizers.show', [
-        'organizer' => $org,
-        'members' => $members,
-        'events' => $events
-    ]);
-});
-
-Route::get('/organizers/{org}/edit', function(Organizer $org) {
-    return view('organizers.edit', [
-        'organizer' => $org
-    ]);
+Route::controller(OrganizerController::class)->group(function () {
+    // TODO: Use Route::resource, but we'll specify the action keywords for now since we don't remember it yet.
+    Route::get('/organizers', 'index');             // page
+    Route::get('/organizers/create', 'create');     // form
+    Route::post('/organizers', 'store');            // form action
+    Route::delete('/organizers/{org}', 'destroy');  // form action
+    Route::patch('/organizers/{org}', 'update');    // form
+    Route::get('/organizers/{org}', 'show');        // page
+    Route::get('/organizers/{org}/edit', 'edit');   // form action
 });
 
 if (env('APP_ENV') == "production") {
