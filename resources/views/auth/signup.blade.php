2a31402aa8642ffba1f91bfdea262bf0d4c042f3
lonewanderer27 <adrianejames27@gmail.com>
2025-01-05 23:18:26 +0800

    feat(signup): show form errors

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -17,7 +17,7 @@
         Signup
     </x-slot:heading>
     <div class="flex items-center h-screen justify-center px-4">
-        <div class="flex flex-col md:flex-row w-full md:w-2/3 md:h-[420px] shadow-xl rounded-xl">
+        <div class="flex flex-col md:flex-row w-full md:w-2/3 md:h-min-[420px] shadow-xl rounded-xl">
             <!-- Left Section: Login Message -->
             <div id="message"
                  class="gap-4 rounded-tl-xl rounded-bl-xl w-full flex flex-col items-center justify-center px-8 py-16 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
@@ -47,7 +47,14 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
                         </svg>
-                        <input name="name" class="grow" placeholder="Enter your Name"/>
+                        <input name="name" class="grow @error('name') input-error @enderror"
+                               placeholder="Enter your Name"
+                               value="{{ old('name') }}"/>
+                        @error('name')
+                        <div class="label">
+                            <span class="label-text-alt text-error">{{ $message }}</span>
+                        </div>
+                        @enderror
                     </label>
 
                     <!-- Email Field -->
@@ -58,7 +65,15 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"/>
                         </svg>
-                        <input name="email" type="email" class="grow" placeholder="Enter your Email"/>
+                        <input name="email" type="email" class="grow @error('email') input-error @enderror"
+                               placeholder="Enter your Email"
+                               value="{{ old('email') }}"
+                        />
+                        @error('email')
+                        <div class="label">
+                            <span class="label-text-alt text-error">{{ $message }}</span>
+                        </div>
+                        @enderror
                     </label>
 
                     <!-- Password Field -->
@@ -69,7 +84,9 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"/>
                         </svg>
-                        <input name="password" type="password" class="grow" placeholder="Enter your Password"/>
+                        <input name="password" type="password" class="grow @error('password') input-error @enderror"
+                               placeholder="Enter your Password"
+                               value="{{ old('password') }}"/>
                         <svg id="eye--icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                              stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -83,6 +100,11 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"/>
                         </svg>
+                        @error('password')
+                        <div class="label">
+                            <span class="label-text-alt text-error">{{ $message }}</span>
+                        </div>
+                        @enderror
                     </label>
 
                     <!-- Confirm Password Field -->
@@ -93,8 +115,10 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"/>
                         </svg>
-                        <input name="password_confirmation" type="password" class="grow"
-                               placeholder="Confirm your Password"/>
+                        <input name="password_confirmation" type="password"
+                               class="grow @error('password_confirmation') input-error @enderror"
+                               placeholder="Confirm your Password"
+                               value="{{ old('password_confirmation') }}"/>
                         <svg id="eye--icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                              stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round"
@@ -108,6 +132,11 @@
                             <path stroke-linecap="round" stroke-linejoin="round"
                                   d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"/>
                         </svg>
+                        @error('password_confirmation')
+                        <div class="label">
+                            <span class="label-text-alt text-error">{{ $message }}</span>
+                        </div>
+                        @enderror
                     </label>
 
                     <div class="flex justify-center mt-5">
